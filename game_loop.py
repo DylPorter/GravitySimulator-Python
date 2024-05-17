9909246e31bcd919e33ba2288a801157dd4fb843
DylPorter <deepeethreeohfive@gmail.com>
2024-05-17 15:03:33 +0800

    update

--- game_loop.py
+++ game_loop.py
@@ -1,24 +1,31 @@
-import pyray
-from logic import Logic
+import pyray, logic
 
 class Window:
     def __init__(self):
-        self.logic = Logic()
-
-        pyray.init_window(800, 450, "Hello")
+        self.window_width = 800
+        self.window_height = 450
         pyray.set_target_fps(60)
 
     def start(self):
-        while not pyray.window_should_close():
+        pyray.init_window(self.window_width, self.window_height, "Hello")
 
-            if pyray.is_mouse_button_down(0):
-                self.logic.positions.append(pyray.get_mouse_position())
+        while not pyray.window_should_close():
+            timestep = pyray.get_frame_time()
+
+            if pyray.is_mouse_button_pressed(0):
+                new_planet = logic.Planet(pyray.get_mouse_position(),   # Position
+                                          15,                           # Radius
+                                          pyray.WHITE,                  # Colour
+                                          pyray.vector2_zero(),         # Velocity
+                                          pyray.vector2_zero())         # Acceleration
+                logic.bodies.append(new_planet)
+
 
             pyray.begin_drawing()
             pyray.clear_background(pyray.BLACK)
 
-            for i in self.logic.positions:
-                pyray.draw_circle_v(i, 20, pyray.WHITE)
+            for i in logic.bodies:
+                pyray.draw_circle_v(i.pos, i.radius, i.colour)
 
             pyray.end_mode_2d()
 
--- logic.py
+++ logic.py
@@ -1,3 +1,39 @@
-class Logic:
-    def __init__(self):
-        self.positions = []
+import pyray, math
+
+class Planet:
+    def __init__(self, pos, radius, colour, v, a):
+        self.pos = pos
+        self.radius = radius
+        self.colour = colour
+        self.v = v
+        self.a = a
+
+        # Consider adding self.density, value between 0 to 1 to multiply the mass by
+
+        self.mass = math.pi * ((self.radius)**2) # Area of a circle
+
+    def update_position(self, timestep):
+        pass
+    
+    def calculate_velocity(self, other, timestep):
+        pass
+
+    def calculate_acceleration(self, other):
+        force = self.find_force(other)
+
+        self.a.x = force.x / self.mass
+        self.a.y = force.y / self.mass
+
+    def find_force(self, other):    # Newton's Law of Universal Gravitation
+        G = 6.674                   # G ~= 6.674 * 10^-11
+
+        x_dist = (other.pos.x - self.pos.x)
+        y_dist = (other.pos.y - self.pos.y)
+        
+        x_force = (G * (self.mass * other.mass)) / math.pow(x_dist, 2)
+        y_force = (G * (self.mass * other.mass)) / math.pow(y_dist, 2)
+
+        return pyray.Vector2(x_force, y_force)
+
+
+bodies = []

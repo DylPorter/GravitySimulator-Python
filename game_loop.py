6febfcb2040a782e4ca6e4d3765905d938cd55b0
DylPorter <deepeethreeohfive@gmail.com>
2024-05-20 21:02:35 +0800

    finished velocity verlet

--- game_loop.py
+++ game_loop.py
@@ -2,24 +2,34 @@ import pyray, logic
 
 class Window:
     def __init__(self):
-        self.window_width = 800
-        self.window_height = 450
+        self.window_width = 1800
+        self.window_height = 1450
         pyray.set_target_fps(60)
 
     def start(self):
         pyray.init_window(self.window_width, self.window_height, "Hello")
 
         while not pyray.window_should_close():
-            timestep = pyray.get_frame_time()
+            if pyray.is_key_down(32):
+                timestep = 0
+            else:
+                timestep = pyray.get_frame_time()
 
+
+            print(timestep)
             if pyray.is_mouse_button_pressed(0):
-                new_planet = logic.Planet(pyray.get_mouse_position(),   # Position
-                                          15,                           # Radius
-                                          pyray.WHITE,                  # Colour
-                                          pyray.vector2_zero(),         # Velocity
-                                          pyray.vector2_zero())         # Acceleration
-                logic.bodies.append(new_planet)
+                new_body = logic.Body(pyray.get_mouse_position(),   # Position
+                                      pyray.vector2_zero(),         # Velocity
+                                      pyray.vector2_zero(),         # Acceleration
+                                      15,                           # Radius
+                                      pyray.WHITE)                  # Colour
+                logic.bodies.append(new_body)
 
+            for body in logic.bodies:
+                for other in logic.bodies:
+                    if body != other:
+                        body.update_position(timestep)
+                        body.calculate_vel_acc(other, timestep)
 
             pyray.begin_drawing()
             pyray.clear_background(pyray.BLACK)
--- logic.py
+++ logic.py
@@ -1,37 +1,46 @@
 import pyray, math
 
-class Planet:
-    def __init__(self, pos, radius, colour, v, a):
+class Body:
+    def __init__(self, pos, v, a, radius, colour):
+        self.prev_pos = pos
         self.pos = pos
-        self.radius = radius
-        self.colour = colour
         self.v = v
         self.a = a
+        self.radius = radius
+        self.colour = colour
 
         # Consider adding self.density, value between 0 to 1 to multiply the mass by
 
         self.mass = math.pi * ((self.radius)**2) # Area of a circle
 
     def update_position(self, timestep):
-        pass
-    
-    def calculate_velocity(self, other, timestep):
-        pass
-
-    def calculate_acceleration(self, other):
+        self.pos.x += (self.v.x * timestep) + (0.5 * self.a.x * math.pow(timestep, 2))
+        self.pos.y += (self.v.y * timestep) + (0.5 * self.a.y * math.pow(timestep, 2))
+        # x(t+dt) = x(t) + v(t)dt + (1/2)(a)(t^2)
+        
+    def calculate_vel_acc(self, other, timestep):
         force = self.find_force(other)
 
-        self.a.x = force.x / self.mass
-        self.a.y = force.y / self.mass
+        new_a_x = force.x / self.mass      # F = m * a (rearrange)
+        new_a_y = force.y / self.mass      # a = F / m
+
+        self.v.x += 0.5 * (self.a.x + new_a_x) * timestep     # v(t+dt) = v(t) + (1/2)at
+        self.v.y += 0.5 * (self.a.y + new_a_y) * timestep
+
+        self.a.x = new_a_x
+        self.a.y = new_a_y
 
     def find_force(self, other):    # Newton's Law of Universal Gravitation
-        G = 6.674                   # G ~= 6.674 * 10^-11
+        G = 66.74                   # G ~= 6.674 * 10^-11
 
-        x_dist = (other.pos.x - self.pos.x)
+        x_dist = (other.pos.x - self.pos.x) # Solve for the triangle
         y_dist = (other.pos.y - self.pos.y)
+        hypo = math.sqrt(math.pow(x_dist, 2) + math.pow(y_dist, 2))
         
-        x_force = (G * (self.mass * other.mass)) / math.pow(x_dist, 2)
-        y_force = (G * (self.mass * other.mass)) / math.pow(y_dist, 2)
+        force = G * (self.mass * other.mass) / math.pow(hypo, 2) # Newton's formula
+        
+        x_force = force * (x_dist / hypo) # Convert to unit vector for the direction
+        y_force = force * (y_dist / hypo)
 
         return pyray.Vector2(x_force, y_force)
 
